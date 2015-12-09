6e0ae9f4008baf2ade14c0d7f3da66e67e899d41
Dennis Kempin <dennis.kempin@gmail.com>
2015-12-09 14:57:55 -0800

    updated fish config to use oh my fish for config

---
+++ fish/before.init.fish
@@ -0,0 +1,1 @@
+set -g Z_SCRIPT_PATH /homebrew/etc/profile.d/z.sh
--- fish/config.fish
+++
@@ -1,5 +0,0 @@
-set -g Z_SCRIPT_PATH /homebrew/etc/profile.d/z.sh
-
-# Load oh-my-fish
-set -gx OMF_PATH ~/.local/share/omf
-source $OMF_PATH/init.fish

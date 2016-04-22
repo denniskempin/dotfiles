0a02324087e004507b350a35ab281158ca3c77d9
Dennis Kempin <dennis.kempin@gmail.com>
2016-04-22 13:14:16 -0700

    Updated EDITOR variables

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -6,6 +6,38 @@ setboard() { export B=--board="$*"; export BOARD="$*"; echo $B; }
 setdevice(){ export DEVICE="$*"; echo "$*" > ~/deviceip; }
 deploy() { emerge-$BOARD "$*" && cros deploy $DEVICE "$*"; }
 
+# Chrome debugging
+chrome_stackwalk() {
+	mkdir -p crash/symbols
+
+	select FILENAME in `ssh $1 'ls /var/spool/crash | grep dmp'`;
+	do
+		FILE=crash/$FILENAME
+		if [ ! -e $FILE ]; then
+			echo "Downloading $FILENAME"
+			scp $1:/var/spool/crash/$FILENAME $FILE
+		fi
+
+		HASH=`minidump_stackwalk -m $FILE | grep 'Module|chrome|' | awk -F'|' '{print $5}'`
+
+		mkdir -p crash/symbols/chrome/$HASH
+		SYM_FILE=crash/symbols/chrome/$HASH/chrome.sym
+		if [ ! -e $SYM_FILE ]; then
+			echo "Generating symbols for $HASH"
+			dump_syms $2/chrome > /tmp/chrome.sym
+			SYM_HASH=`head -n1 /tmp/chrome.sym | awk '{print $4}'`
+
+			if [ ! "$HASH" = "$SYM_HASH" ]; then
+				echo "Local chrome binary does not match crashed binary."
+				exit
+		 	fi
+		 	cp /tmp/chrome.sym $SYM_FILE
+		fi
+
+		minidump_stackwalk $FILE crash/symbols
+		exit
+	done
+}
 
 # GIT history editing methods
 CL_IN_EDIT=NONE
@@ -22,8 +54,7 @@ gitlog() { gitlogall | head -n 40 }
 
 # Android workflow
 deploy_test() {
-  scp out/target/product/$TARGET_PRODUCT/data/nativetest/$1/$1 $2:/home/chronos/user/android-data/data
-  ssh $2 "chmod +x /home/chronos/user/android-data/data/$1"
+  rsync -v --chmod=755 out/target/product/$TARGET_PRODUCT/data/nativetest/$1/$1 $2:/home/root/\*/android-data/data
 }
 run_test() { ssh $2 "android-sh -c \"/data/$1\"" }
 bdr_test() { m $1 && deploy_test $1 $2 && run_test $1 $2 }

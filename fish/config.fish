bed680f22a98a8106804aa6392f88bd777957866
Dennis Kempin <dennis.kempin@gmail.com>
2022-03-06 15:18:55 -0800

    Make exa / zoxide optional

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
 starship init fish | source
-zoxide init fish | source
+if command -v zoxide &> /dev/null
+    zoxide init fish | source
+end
 
-alias ls="exa"
-alias ll="exa -l"
-alias la="exa -la"
+if command -v exa &> /dev/null
+    alias ls="exa"
+    alias ll="exa -l"
+    alias la="exa -la"
+end
 
 # Use hyper+t and hyper+s for history and directory search
 fzf_configure_bindings --history=\e\ct --directory=\e\cs --git_status --git_log

c255a87ca15042b2009271b31f0d5702e17e1e63
Dennis Kempin <denniskempin@google.com>
2022-04-19 13:12:01 -0700

    add some git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,17 @@
 set dotfiles_dir (realpath (dirname (realpath (status --current-filename)))/..)
 
+fish_add_path -P ~/go/bin
+fish_add_path -P ~/.cargo/bin
+fish_add_path -P $dotfiles_dir/containers
+source ~/local/paths.fish
+
 starship init fish | source
 
-if command -v zoxide &> /dev/null
+if command -v zoxide &>/dev/null
     zoxide init fish | source
 end
 
-source {$dotfiles_dir}/fish/iterm2_shell_integration.fish
-
-if command -v exa &> /dev/null
+if command -v exa &>/dev/null
     alias ls="exa"
     alias ll="exa -l"
     alias la="exa -la"
@@ -23,6 +26,43 @@ export LESS="--mouse"
 # Use nvim as default editor
 export EDITOR="nvim"
 
-set PATH $PATH $HOME/go/bin
-set PATH $PATH $HOME/.cargo/bin
-set PATH $PATH $dotfiles_dir/containers
+# extend 'git push origin' abbreviation for gerrit
+alias gpom="git push origin HEAD:refs/for/main"
+alias gpoc="git push origin HEAD:refs/for/chromeos"
+
+# git 'uncommit' - along the lines of 'hg uncommit'
+alias guc="git reset HEAD~ && git commit --reuse-message=HEAD@{1} --allow-empty"
+
+
+function git_amend_to
+    if git log HEAD..@{u} --oneline | grep '' >/dev/null
+        echo 'Branch needs to rebase first.'
+        return
+    end
+
+    set branch (git rev-parse --abbrev-ref HEAD)
+    set target (git log @{u}..HEAD --oneline | fzf | awk '{print $1}')
+    echo 'Amending to:'
+    echo
+    git log -n 1 --color $target
+    echo
+    echo 'Amending with'
+    echo
+    git diff
+    git diff --cached
+    echo
+
+    git stash -q
+    git checkout -q "$target"
+    if ! git stash apply -q
+        echo 'Diff does not apply cleanly. Reverting.'
+        git checkout -fq "$branch"
+        git stash pop -q
+    end
+
+    git commit --amend -a --no-edit
+    git rebase --onto HEAD $target $branch
+    git stash drop -q
+end
+
+alias gat="git_amend_to"
